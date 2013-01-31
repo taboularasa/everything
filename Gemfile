2982dafb4d6c68e55387a1ddf08948060f87aca1
David Elliott <david@hadto.net>
2013-01-30 16:56:22 -0800

    added getter setter and field for hstore authors

--- Gemfile
+++ Gemfile
@@ -7,6 +7,9 @@ gem 'rails', '3.2.11'
 
 gem 'pg'
 
+group :development do
+  gem 'better_errors'
+end
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ActiveRecord::Base
-  attr_accessible :category, :description, :name, :price
+  attr_accessible :category, :description, :name, :price, :author
+
+  def author
+    properties && properties["author"]
+  end
+
+  def author=(value)
+    self.properties = (properties || {}).merge("author" => value)
+  end
 end
