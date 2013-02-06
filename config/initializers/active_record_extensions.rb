35a38064ce0707ca535650361e6a295b4feeaaa0
David Elliott <david@hadto.net>
2013-02-06 15:52:32 -0800

    removed references to product.price in views, changed how hstore_accessor is loaded

---
+++ config/initializers/active_record_extensions.rb
@@ -0,0 +1,1 @@
+require "hstore_accessor"
--- lib/hstore_accessor.rb
+++ lib/hstore_accessor.rb
@@ -1,16 +1,17 @@
+# include from an initializer
+
 module HstoreAccessor
   def self.included(base)
     base.extend(ClassMethods)
   end
-
+  
   module ClassMethods
     def hstore_accessor(hstore_attribute, *keys)
       Array(keys).flatten.each do |key|
         define_method("#{key}=") do |value|
-          send("#{hstore_attribute}=", (send(hstore_attribute) || {}).mrege(key.to_s => value))
+          send("#{hstore_attribute}=", (send(hstore_attribute) || {}).merge(key.to_s => value))
           send("#{hstore_attribute}_will_change!")
         end
-        
         define_method(key) do
           send(hstore_attribute) && send(hstore_attribute)[key.to_s]
         end
@@ -18,3 +19,5 @@ module HstoreAccessor
     end
   end
 end
+
+ActiveRecord::Base.send(:include, HstoreAccessor)
